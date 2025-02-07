858a5155d0a68323921867701f8e88f4aacfd592
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-07 17:33:19 +0530

    minor changes

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -1,1 +1,18 @@
-const Gameboard = require("../module/gameboard.js")
+const {Gameboard} = require("../module/gameboard.js")
+
+describe("gameboard.js",() => {
+
+    let gameboard;
+    beforeEach(() => {
+        gameboard = new Gameboard()
+    });
+    
+    test("gameboard class exist",() => {
+        expect(Gameboard).toBeDefined();
+    })
+
+
+    test("gameboard return object",() => {
+        expect(typeof gameboard).toBe("object")
+    })
+})
