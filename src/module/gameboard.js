8de25b98027bba1ba9025f55babecebbe5addc38
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-08 19:16:52 +0530

    changed how gamboard is created & added a print function

--- src/module/gameboard.js
+++ src/module/gameboard.js
@@ -1,19 +1,14 @@
 class Gameboard{
     constructor(){
-        this.gameboard = [
-            [0,0,0,0,0,0,0,0,0,0],  //0
-            [0,0,0,0,0,0,0,0,0,0],  //1
-            [0,0,0,0,0,0,0,0,0,0],  //2
-            [0,0,0,0,0,0,0,0,0,0],  //3
-            [0,0,0,0,0,0,0,0,0,0],  //4
-            [0,0,0,0,0,0,0,0,0,0],  //5
-            [0,0,0,0,0,0,0,0,0,0],  //6
-            [0,0,0,0,0,0,0,0,0,0],  //7
-            [0,0,0,0,0,0,0,0,0,0],  //8
-            [0,0,0,0,0,0,0,0,0,0],  //9
-        ]
+        this.gameboard = Array.from({length:10},() => Array(10).fill(0));
+    }
+
+    print(){
+        console.log("GameBoard~");
+        this.gameboard.forEach(row => console.log(row.join(" ")));
     }
 };
+
 
 module.exports = {
     Gameboard
