51ffc530f31c87aafd61af193115547181bdd185
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-11 10:42:12 +0530

    gameBoard object changed to board

--- src/module/gameboard.js
+++ src/module/gameboard.js
@@ -1,14 +1,13 @@
 class Gameboard{
     constructor(){
-        this.gameboard = Array.from({length:10},() => Array(10).fill(0));
+        this.board = Array.from({length:10},() => Array(10).fill(0));
     }
 
     print(){
         console.log("GameBoard~");
-        this.gameboard.forEach(row => console.log(row.join(" ")));
+        this.board.forEach(row => console.log(row.join(" ")));
     }
 };
-
 
 module.exports = {
     Gameboard
