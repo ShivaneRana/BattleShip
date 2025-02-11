aea176e8fb2ed220f47a94ef3785ca4b5f3cef2a
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-11 12:04:06 +0530

    placeShip function created and edge case dealt with

--- src/module/gameboard.js
+++ src/module/gameboard.js
@@ -7,6 +7,28 @@ class Gameboard{
         console.log("GameBoard~");
         this.board.forEach(row => console.log(row.join(" ")));
     }
+
+    placeShip(start,end,name){
+        if(!(Array.isArray(start))){
+            throw new Error("start should be an array");
+        }
+
+        if(start.length !== 2){
+            throw new Error("array length should be 2");
+        }
+
+        if(!(Array.isArray(end))){
+            throw new Error("end should be an array");
+        }
+
+        if(end.length !== 2){
+            throw new Error("array length should be 2");
+        }
+
+        if(typeof name !== "string"){
+            throw new Error("name should be a string");
+        }
+    }
 };
 
 module.exports = {
