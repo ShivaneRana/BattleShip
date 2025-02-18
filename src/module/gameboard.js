259b5dce1865aefe486cce8d5594f48cf666a610
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-18 12:15:03 +0530

    code refactored

--- src/module/gameboard.js
+++ src/module/gameboard.js
@@ -4,11 +4,11 @@ export class Gameboard {
   constructor() {
     this.board = Array.from({ length: 10 }, () => Array(10).fill(0));
     this.allShip = {
-      carrier: new Ship("carrier"),
-      destroyer: new Ship("destroyer"),
-      cruiser: new Ship("cruiser"),
-      battleship: new Ship("battleship"),
-      submarine: new Ship("submarine"),
+      carrier: new Ship("carrier"),       //5
+      battleship: new Ship("battleship"), //4
+      cruiser: new Ship("cruiser"),       // 3
+      submarine: new Ship("submarine"),   //3
+      destroyer: new Ship("destroyer"),   //2
     };
     this.visited = new Set();
   }
@@ -16,7 +16,7 @@ export class Gameboard {
   print() {
     console.log("GameBoard~");
     this.board.forEach((row) => {
-      console.log(row.join(" "))
+      console.log(row.join("   "))
     });
   }
 
@@ -27,44 +27,37 @@ export class Gameboard {
     const x1 = end[0];  // end position x axis
     const y1 = end[1];  // end position y axis
 
-    if (!Array.isArray(start) || !Array.isArray(end)) {
-      throw new Error("start and end should be arrays");
-    }
+    if (!Array.isArray(start) || !Array.isArray(end) || start.length !== 2 || end.length !== 2){
+      throw new Error("start and end should be arrays of length 2")
+    };
 
-    if (start.length !== 2 || end.length !== 2) {
-      throw new Error("array length should be 2");
-    }
+    if (typeof name !== "string") throw new Error("name should be a string");
 
-    if (typeof name !== "string") {
-      throw new Error("name should be a string");
-    }
+    if (x !== x1 && y !== y1) return false;
+    if (x === x1 && y === y1) return false;
 
-    if (x !== x1 && y !== y1) {
-      return false;
-    }
-
-    if (x === x1 && y === y1) {
-      return false;
-    }
-
-    if (x === x1 && y !== y1) { // vertical placement
+    // horizontal placement
+    if (x === x1 && y !== y1) {
       for (let i = y; i <= y1; i++) {
         if (this.board[x][i] !== 0) {
           return false;
         }
       }
+
       for (let i = y; i <= y1; i++) {
         this.board[x][i] = ship.symbol;
       }
       return true;
     }
 
-    if (y === y1 && x !== x1) { // horizontal placement
+    // vertical placement
+    if (y === y1 && x !== x1) {
       for (let i = x; i <= x1; i++) {
         if (this.board[i][y1] !== 0) {
           return false;
         }
       }
+
       for (let i = x; i <= x1; i++) {
         this.board[i][y1] = ship.symbol;
       }
