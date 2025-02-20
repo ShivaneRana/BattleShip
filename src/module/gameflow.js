243c859fe9c215682e8e9def943b5b5bec938091
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-20 19:18:59 +0530

    start function created

--- src/module/gameflow.js
+++ src/module/gameflow.js
@@ -1,9 +1,14 @@
 import { RealPlayer,ComputerPlayer } from "./player.js";
 import { getName } from "../index.js";
+import { Render } from "./dom.js";
 
 export class Gameflow{
     constructor(){
-        this.player1 = new RealPlayer(getName());
-        this.player2 = new ComputerPlayer("CPU");
+        this.player1 = new RealPlayer(getName());   //user
+        this.player2 = new ComputerPlayer("CPU");   //computer
+    }
+
+    start(){
+        Render.startScreen();
     }
 }
