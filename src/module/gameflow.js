20f3dd3ac394662d7212503dd3ef6ef781d4e331
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-20 15:50:42 +0530

    getName imported

---
+++ src/module/gameflow.js
@@ -0,0 +1,9 @@
+import { RealPlayer,ComputerPlayer } from "./player.js";
+import { getName } from "../index.js";
+
+export class Gameflow{
+    constructor(){
+        this.player1 = new RealPlayer(getName());
+        this.player2 = new ComputerPlayer("CPU");
+    }
+}
