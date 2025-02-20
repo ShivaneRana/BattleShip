26529fdf11cf50ef0c75237cdf36935055870ab8
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-20 15:49:09 +0530

    getName funciton for returing name inserted by the user

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import "./style.css";
+import { Gameflow } from "./module/gameflow.js";
 
 const startButton = document.querySelector(".startButton");
+const inputName = document.querySelector(".inputName");
 
 startButton.addEventListener("click",() => {
     document.body.textContent = "";
     document.body.style.backgroundImage = "none";
     document.body.style.backgroundColor = "#ffffff";
+    new Gameflow();
 })
+
+export function getName(){
+    return inputName.value;
+}
