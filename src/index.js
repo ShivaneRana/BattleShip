614315bd67431466dfc70969921ec6685fa13cb0
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-20 11:35:14 +0530

    startButton and functionality added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const startButton = document.querySelector(".startButton");
+
+startButton.addEventListener("click",() => {
+    document.body.textContent = "";
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "#ffffff";
+})
