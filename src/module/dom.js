498f39d43899fc2bd3c6260bddcef5040d905ad6
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-22 14:44:11 +0530

    howToButton and randomButton added with styling

--- src/module/dom.js
+++ src/module/dom.js
@@ -6,21 +6,30 @@ export const Render = (function(){
     //create Element
     const mainScreen = document.createElement("div");
     const header = document.createElement("h1");
-    const board = document.createElement("div");
+    const board = document.createElement("div"); 
+    const buttonHolder = document.createElement("div");
+    const randomButton = document.createElement("button");
+    const howToButton = document.createElement("button");
 
     //assign classes
     mainScreen.classList.add("startScreen");
+    buttonHolder.classList.add("buttonHolder");
     header.classList.add("header");
     board.classList.add("board");
+    
 
     //assign values
     header.textContent = "Place your ships!";
+    howToButton.textContent = "How to Play?";
+    randomButton.textContent = "Randomize";
     renderTiles(array,board);
 
     
     //append value
     mainScreen.append(header);
     mainScreen.append(board);
+    buttonHolder.append(howToButton,randomButton);
+    mainScreen.append(buttonHolder);
     document.body.append(mainScreen);
 
   }
