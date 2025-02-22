b5b1e5881a94db4c958bfa9dcf37077d77e5159d
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-22 14:03:46 +0530

    displayStartScreen and renderTile function created

--- src/module/dom.js
+++ src/module/dom.js
@@ -1,3 +1,43 @@
-export function Render(){
+import "./dom.css";
+
+export const Render = (function(){
+  
+  function displayStartScreen(array){
+    //create Element
+    const mainScreen = document.createElement("div");
+    const header = document.createElement("h1");
+    const board = document.createElement("div");
+
+    //assign classes
+    mainScreen.classList.add("startScreen");
+    header.classList.add("header");
+    board.classList.add("board");
+
+    //assign values
+    header.textContent = "Place your ships!";
+    renderTiles(array,board);
+
+    
+    //append value
+    mainScreen.append(header);
+    mainScreen.append(board);
+    document.body.append(mainScreen);
+
+  }
+
+  return{
+    displayStartScreen
+  }
+
+}());
+
+function renderTiles(array,holder){
+  array.forEach(arr => {
+      arr.forEach(element => {
+          const div = document.createElement("div");
+          div.classList.add("tiles");
+          holder.append(div);
+      })
+  })
 
 }
