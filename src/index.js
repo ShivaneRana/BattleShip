dfb0c73cfde6a2a99cb1bdd9b696aa7f53f04b79
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-24 21:54:41 +0530

    Tutorial button added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
+import { renderTutorialScreen } from "./module/dom.js";
 
+const tutorialButton = document.querySelector(".tutorialButton");
 const startButton = document.querySelector(".startButton");
 const inputName = document.querySelector(".inputName");
 
@@ -7,8 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
   inputName.value = "";
 });
 
-startButton.addEventListener("click", () => {});
+startButton.addEventListener("click", () => {
 
+});
+
+tutorialButton.addEventListener("click",() => {
+  renderTutorialScreen();
+})
+
+// return the player input to the game class
 export function getName() {
   return inputName.value;
 }
