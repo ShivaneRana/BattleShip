8b44042775a7bebd8301e0d249b6a791668af708
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-24 21:56:48 +0530

    gameflow.js renamed to game.js

--- src/module/gameflow.js
+++
@@ -1,9 +0,0 @@
-import { RealPlayer, ComputerPlayer } from "./player.js";
-import { getName } from "../index.js";
-
-export class Gameflow {
-  constructor() {
-    this.player1 = new RealPlayer(getName()); //user
-    this.player2 = new ComputerPlayer("CPU"); //computer
-  }
-}
