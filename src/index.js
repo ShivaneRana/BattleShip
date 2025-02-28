dc043b826501004c7f27f6fbf4fb92880597f1c5
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-28 16:54:33 +0530

    tutorial and start button now functional

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import { Game } from "./module/game.js";
 const tutorialButton = document.querySelector(".tutorialButton");
 const startButton = document.querySelector(".startButton");
 const inputName = document.querySelector(".inputName");
+const introContainer = document.querySelector(".introContainer");
 
 document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
 startButton.addEventListener("click", () => {
+    const name = inputName.value;
+    introContainer.remove();
+    Game.showPlacementScreen(name);
 });
 
 tutorialButton.addEventListener("click", () => {
-
+    Game.showTutorialScreen();
 });
