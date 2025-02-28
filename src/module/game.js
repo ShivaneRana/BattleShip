748e68bbba8bf6ef139fc8719590cc3c7c89c7ee
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-28 19:58:34 +0530

    showGameScreen function created

--- src/module/game.js
+++ src/module/game.js
@@ -2,8 +2,9 @@ import { RealPlayer, ComputerPlayer } from "./player.js";
 import { Render, renderTutorialScreen } from "./dom.js";
 
 export const Game = (function () {
-  let player1; //user
-  let player2; //CPU
+  let playerName;
+  let player1 = new RealPlayer(playerName);
+  let player2 = new ComputerPlayer("CPU");
 
   function showTutorialScreen() {
     renderTutorialScreen();
@@ -11,14 +12,21 @@ export const Game = (function () {
 
   // this is where the player will place their ship
   function showPlacementScreen(name) {
-    player1 = new RealPlayer((name = "Player"));
+    playerName = name;
+    player1 = new RealPlayer(playerName);
     player2 = new ComputerPlayer("CPU");
-    const board = player1.gameboard.board;
-    Render.placementScreen(board);
+    Render.placementScreen(player1.gameboard.board);
+  }
+
+
+
+  function showGameScreen(){
+    Render.gameScreen(player1.gameboard.board,player2.gameboard.board);
   }
 
   return {
     showTutorialScreen,
     showPlacementScreen,
+    showGameScreen
   };
 })();
