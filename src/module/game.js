d996dbcbaf38638fea2c88888f57db6e936acadb
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-03-01 17:28:49 +0530

    minor changes to the code while development

--- src/module/game.js
+++ src/module/game.js
@@ -3,8 +3,8 @@ import { Render, renderTutorialScreen } from "./dom.js";
 
 export const Game = (function () {
   let playerName;
-  let player1;
-  let player2;
+  let player1 = new RealPlayer("Shivane");
+  let player2 = new ComputerPlayer("CPU");
 
   function showTutorialScreen() {
     renderTutorialScreen();
@@ -12,15 +12,16 @@ export const Game = (function () {
 
   // this is where the player will place their ship
   function showPlacementScreen(name) {
-    playerName = name;
-    player1 = new RealPlayer(playerName);
-    player2 = new ComputerPlayer("CPU");
-    Render.placementScreen(player1);
+    // playerName = name;
+    // player1 = new RealPlayer(playerName);
+    // player2 = new ComputerPlayer("CPU");
+    // Render.placementScreen(player1);
   }
 
   function showGameScreen(){
     document.body.textContent = "";
     player2.gameboard.placeShipRandomly();// place ship randomly on the enemy board;
+    player1.gameboard.placeShipRandomly();
     //append the returned element to DOM
     document.body.append(Render.gameScreen(player1,player2));
   }
