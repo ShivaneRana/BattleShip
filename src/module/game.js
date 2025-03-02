02b8508236b0a7a14bd04bd4f8401d1dd107ae8a
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-03-02 11:53:56 +0530

    restart function created

--- src/module/game.js
+++ src/module/game.js
@@ -1,32 +1,39 @@
 import { RealPlayer, ComputerPlayer } from "./player.js";
-import { Render, renderTutorialScreen } from "./dom.js";
+import { Render, renderBanner, renderTutorialScreen } from "./dom.js";
 
 export const Game = (function () {
   let playerName;
-  let player1 = new RealPlayer("Shivane");
-  let player2 = new ComputerPlayer("CPU");
-
+  let player1;
+  let player2;
+  // player2.gameboard.placeShipRandomly(); // place ship randomly on the enemy board;
+  // player1.gameboard.placeShipRandomly();
   function showTutorialScreen() {
     renderTutorialScreen();
   }
 
   // this is where the player will place their ship
   function showPlacementScreen(name) {
-    // playerName = name;
-    // player1 = new RealPlayer(playerName);
-    // player2 = new ComputerPlayer("CPU");
-    // Render.placementScreen(player1);
+    playerName = name;
+    Render.placementScreen(player1);
   }
 
   function showGameScreen() {
     document.body.textContent = "";
-    player2.gameboard.placeShipRandomly(); // place ship randomly on the enemy board;
-    player1.gameboard.placeShipRandomly();
+    
+    player1 = new RealPlayer("player");
+    player2 = new ComputerPlayer("CPU");
     //append the returned element to DOM
+    player1.gameboard.placeShipRandomly();
+    player2.gameboard.placeShipRandomly();
     document.body.append(Render.gameScreen(player1, player2));
   }
 
+  function restart(){
+    renderBanner();
+  }
+
   return {
+    restart,
     showTutorialScreen,
     showPlacementScreen,
     showGameScreen,
